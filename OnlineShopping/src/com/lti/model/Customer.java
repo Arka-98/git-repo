34ab6f68841c048ee08c12c03538bef18adb8470
Arkadipta <Arkadipta@DESKTOP-1KCR77B>
2020-10-15 15:49:45 +0530

    Updated customer class

--- OnlineShopping/src/com/lti/model/Customer.java
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -3,20 +3,30 @@ package com.lti.model;
 public class Customer {
 
 	private int custId;
-	private int custName;
+	private String custName;
+	private int custPhone;
 	public Customer() {
 		
 	}
+	
+	public int getCustPhone() {
+		return custPhone;
+	}
+
+	public void setCustPhone(int custPhone) {
+		this.custPhone = custPhone;
+	}
+
 	public int getCustId() {
 		return custId;
 	}
 	public void setCustId(int custId) {
 		this.custId = custId;
 	}
-	public int getCustName() {
+	public String getCustName() {
 		return custName;
 	}
-	public void setCustName(int custName) {
+	public void setCustName(String custName) {
 		this.custName = custName;
 	}
 	

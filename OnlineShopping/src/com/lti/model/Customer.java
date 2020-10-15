4fdcf0292077478ff3f9791a12ee206a063bf0f1
Arkadipta <Arkadipta@DESKTOP-1KCR77B>
2020-10-15 15:44:51 +0530

    First Commit

---
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -0,0 +1,23 @@
+package com.lti.model;
+
+public class Customer {
+
+	private int custId;
+	private int custName;
+	public Customer() {
+		
+	}
+	public int getCustId() {
+		return custId;
+	}
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+	public int getCustName() {
+		return custName;
+	}
+	public void setCustName(int custName) {
+		this.custName = custName;
+	}
+	
+}

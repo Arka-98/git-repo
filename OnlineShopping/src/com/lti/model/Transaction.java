4168eb309636ea60fa7422b05a37b1cf22d4d406
Arkadipta <Arkadipta@DESKTOP-1KCR77B>
2020-10-16 09:34:58 +0530

    Added transaction class

--- OnlineShopping/src/com/lti/model/Transaction.java
+++ OnlineShopping/src/com/lti/model/Transaction.java
@@ -2,4 +2,25 @@ package com.lti.model;
 
 public class Transaction {
 
+	private int transacId;
+	private int transacDate;
+	private int transacAmount;
+	public int getTransacId() {
+		return transacId;
+	}
+	public void setTransacId(int transacId) {
+		this.transacId = transacId;
+	}
+	public int getTransacDate() {
+		return transacDate;
+	}
+	public void setTransacDate(int transacDate) {
+		this.transacDate = transacDate;
+	}
+	public int getTransacAmount() {
+		return transacAmount;
+	}
+	public void setTransacAmount(int transacAmount) {
+		this.transacAmount = transacAmount;
+	}
 }

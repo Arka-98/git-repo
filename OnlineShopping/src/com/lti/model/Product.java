9472ec3120b5e50450ba2edeea05487b07d0f101
Arkadipta <Arkadipta@DESKTOP-1KCR77B>
2020-10-16 09:27:28 +0530

    Added product class

---
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -0,0 +1,31 @@
+package com.lti.model;
+
+public class Product {
+
+	private int productId;
+	private int productName;
+	private int productDesc;
+	
+	public Product() {
+		
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getProductName() {
+		return productName;
+	}
+	public void setProductName(int productName) {
+		this.productName = productName;
+	}
+	public int getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(int productDesc) {
+		this.productDesc = productDesc;
+	}
+	
+}
